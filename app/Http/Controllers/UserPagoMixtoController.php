153214471bacbef52994831e098228a9480bf129
sebastotustuus <32949058+sebastotustuus@users.noreply.github.com>
2019-03-01 23:40:13 -0500

    Add files via upload

---
+++ app/Http/Controllers/UserPagoMixtoController.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+
+class UserPagoMixtoController extends Controller
+{
+    
+    //Petición y codificación URL
+    public function getJson (Request $request){
+        
+        /*
+            ===Construción del Nonce y declaración de Variables para el Trankey====
+        */
+        if (function_exists("random_bytes")) {
+            $nonce = bin2hex(random_bytes(16));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $nonce = bin2hex(openssl_random_pseudo_bytes(16));
+        } else {
+            $nonce = mt_rand(0, mt_getrandmax());
+        }
+
+        /*
+            ===Fin====
+        */
+
+
+        $nonceBase64 = base64_encode($nonce);
+        $secretKey = "024h1IlD";
+        $seed = date("c");
+        $tranKey = base64_encode(sha1($nonce . $seed . $secretKey, true));
+        
+    
+        $request = array(
+            "auth" => array(
+                        "login" => "6dd490faf9cb87a9862245da41170ff2",
+                        "seed" => $seed,
+                        "nonce" => $nonceBase64,
+                        "tranKey" => $tranKey
+                    ),
+            "buyer" => array(
+                    "document" => "1040046087",
+                    "documentType" => "CC",
+                    "name" => "Pablo Andrés",
+                    "surname" => "Muñoz Osorio",
+                    "email" => "paban@correoejemplo.com",
+                    "address" => array(
+                        "street" => "Calle 20 # 6 -63",
+                        "city" => "Medellin",
+                        "country" => "Colombia"
+                    ),
+            ),
+            
+            "payment" => array(
+                "reference" => "5976030f5575d",
+                "description" => "Pago básico de prueba",
+                "amount" => 
+                        array(
+                            "currency" => "COP",
+                            "total" => "10000"
+                        ),
+                "allowPartial" => true
+                ),
+            "expiration" => date("c", strtotime("+1 hour")),
+            "returnUrl" => "https://dev.placetopay.com/redirection/sandbox/session/5976030f5575d",
+            "ipAddress" =>"127.0.0.1",
+            "userAgent" => "PlacetoPay Sandbox"    
+        );
+
+           //dd($request);
+            /*
+                * Conexión por cURL
+                * ¡Códico sin fines comerciales!            
+            */
+            $link = "https://test.placetopay.com/redirection/api/session";
+            $ch = curl_init($link);
+            $datos_json = json_encode($request);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $datos_json);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "User-Agent: cUrl Testing"));
+           
+            $result = curl_exec($ch);
+            $response = json_decode($result, true);
+            $idRequest = $response['requestId'];
+           
+            $processUrl = $response['processUrl'];
+            //dd($processUrl);
+            //dd($idRequest);
+            return view("usuarios2")->with("processUrl", $processUrl);
+    }
+        
+}
+
+
+
+
---
+++ storage/framework/views/94865a8166235c88083bacecc0404c5d48d50d03.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/styles.css">
+    <title>formulario Pago</title>
+</head>
+<body>
+
+<div class="contenedor">
+    <h1>Formulario de Información</h1>
+        <form method="get" action="<?php echo e($processUrl); ?>">
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">Nombre</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Nombre" name="nombre" required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="inputPassword4">Apellido</label>
+                    <input type="text" class="form-control" id="inputPassword4" placeholder="Apellido" name="apellido" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputAddress">Dirección</label>
+                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" name="direccion" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputAddress2">Correo</label>
+                  <input type="email" class="form-control" id="inputAddress2" placeholder="Email" name="correo" required>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-4">
+                    <label for="inputCity">Ciudad</label>
+                    <input type="text" class="form-control" id="inputCity" name="ciudad" required>
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputZip">Pais</label>
+                    <input type="text" class="form-control" id="inputZip" name="pais" required>
+                  </div>
+                </div>
+               
+                <button type="submit" class="btn btn-primary" name="boton">Enviar</button>
+
+            <?php echo e(csrf_field()); ?>    
+            </form>
+</div>
+
+
+
+    
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
